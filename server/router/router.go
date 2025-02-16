89c1dd2810c71cf5a61ec2a401b4594287aef66b
Karol Kącki <karol.kacki2@gmail.com>
2025-02-16 14:23:36 +0100

    cors policy setup

--- server/router/router.go
+++ server/router/router.go
@@ -3,7 +3,9 @@ package router
 import (
 	"server/internal/user"
 	"server/internal/ws"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,19 @@ var r_eng *gin.Engine
 
 func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 	r_eng = gin.Default()
+
+	// set up cors
+	r_eng.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"POST","GET"},
+		AllowHeaders: []string{"Content-Type"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func (origin string) bool  {
+			return origin == "http://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	// user endpoints
 	r_eng.POST("/signup", userHandler.CreateUser)
