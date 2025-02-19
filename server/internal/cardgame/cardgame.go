e92044254bb204aeae120f94c7c65f0a82af738c
Karol Kącki <karol.kacki2@gmail.com>
2025-02-19 21:23:29 +0100

    typo fix

--- server/internal/cardgame/cardgame.go
+++ server/internal/cardgame/cardgame.go
@@ -10,6 +10,6 @@ type Card struct {
 type Deck struct {
 	Id          int64  `json:"id" db:"id"`
 	Name        string `json:"name" db:"name"`
-	Description string `json:"descriiption" db:"description"`
+	Description string `json:"description" db:"description"`
 	AuthorId    int64  `json:"authorId" db:"authorId"`
 }
