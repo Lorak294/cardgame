c79a20ec93a95ebf3f2838b2239208bfe31a3c55
Karol Kącki <karol.kacki2@gmail.com>
2025-02-19 21:23:18 +0100

    cardgame data models

---
+++ server/internal/cardgame/cardgame.go
@@ -0,0 +1,15 @@
+package cardgame
+
+type Card struct {
+	Id     int64  `json:"id" db:"id"`
+	Text   string `json:"text" db:"text"`
+	Type   int8   `json:"type" db:"type"`
+	DeckId int64  `json:"deckId" db:"deckId"`
+}
+
+type Deck struct {
+	Id          int64  `json:"id" db:"id"`
+	Name        string `json:"name" db:"name"`
+	Description string `json:"descriiption" db:"description"`
+	AuthorId    int64  `json:"authorId" db:"authorId"`
+}
